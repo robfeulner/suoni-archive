e121f61c13ac50ce1ea141cc24e1e98cc751b17f
Rob <ucw_feedback2002@yahoo.ca>
2023-04-20 13:13:44 -0400

    adding Cors to index.js again

--- server/index.js
+++ server/index.js
@@ -3,14 +3,24 @@
 // import the needed node_modules.
 const express = require("express");
 const morgan = require("morgan");
-// const cors = require("cors");
+const cors = require("cors");
+// const helmet = require("helmet");
 // const port = 8888;
 const port = process.env.PORT || 8888;
 
 const app = express();
-// const helmet = require("helmet");
 
 require("dotenv").config();
+
+const cors = require("cors")
+  .use(cors())
+  .use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods: *");
+    res.header("Allow: *");
+    next();
+  });
 
 const {
   getEvents,
